326c72a73760c888e3fab6362d588e367072156d
raed.khalaf <raed.khalaf@apprenticefs.com>
2023-10-17 18:41:47 +0300

    REST APIs

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 const server = Bun.serve({
     port: process.env.PORT || 5000,
-    fetch() {
-        return new Response("Hello world!")
+    fetch(req) {
+        const url = new URL(req.url);
+        switch (url.pathname) {
+            case '/':
+                return new Response('Home Page');
+            case '/users':
+                return new Response(JSON.stringify([{name: 'John Doe', age: 30}]));
+            case '/posts':
+                return new Response(JSON.stringify([{title: 'Hello World', body: 'Lorem ipsum dolor sit amet'}]));
+            default:
+                return new Response('Not Found', {status: 404})
+        }
     }
 })
 

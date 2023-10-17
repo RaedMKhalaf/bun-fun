9c3d86efbd3a18936ebdc335368fdd2174118edb
raed.khalaf <raed.khalaf@apprenticefs.com>
2023-10-17 18:31:55 +0300

    Server - Hello world

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+const server = Bun.serve({
+    port: 3000,
+    fetch() {
+        return new Response("Hello world!")
+    }
+})
+
+console.log(`Server running at ${server.port}`)

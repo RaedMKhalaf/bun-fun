9c228ca72e37e2b4fb68b829c5df3db1763bb61d
raed.khalaf <raed.khalaf@apprenticefs.com>
2023-10-26 08:55:27 +0300

    test

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const server = Bun.serve({
     port: process.env.PORT || 5000,
     fetch() {
-        return new Response("Hello world!")
+        return new Response("Hello world! v2")
     }
 })
 
